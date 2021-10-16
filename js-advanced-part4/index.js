18e31f8bc59f3f80803b16284761952738430bb2
Kartik Verma <vkartik2k@gmail.com>
2021-10-16 21:52:55 +0530

    todo

---
+++ js-advanced-part4/index.js
@@ -0,0 +1,56 @@
+console.log("Advanced JS Part - 4")
+
+// spread and rest operators
+
+// let a = [1,2 ,3 , 4]
+
+// let b = [5, 6, 7]
+
+// let cp = [...a]
+
+// a.push(5)
+
+// console.log(cp)
+
+// let namea = ["Ram", "singh"]
+
+// function print(firstName, lastName, address) {
+//     console.log(`Hello ${lastName} ${firstName}`)
+// }
+
+// print(...namea, "Delhi")
+// print(namea[0], namea[1], "delhi")
+
+// a = [10, ...a]
+// let c = []
+
+// a.forEach((e) => c.push(e))
+// b.forEach((e) => c.push(e))
+
+// let c = [...a, ...b, 8]
+
+// rest
+
+// function add(a, b, ...arr) {
+//     console.log(a)
+//     console.log(b)
+//     console.log(arr)
+
+//     return arr.reduce((s, e) => s+e, 0);
+// } 
+
+// let sum = add(1, 2, 3, 4, -10);
+
+// console.log(sum);
+
+// let c = a + b;
+
+// console.log(c)
+
+// JS is a synchronous and single threaded language
+
+// callbacks in js
+
+// scope in js
+
+// closure in js
